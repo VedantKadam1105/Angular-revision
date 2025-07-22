71901c31b9be2f65e554c085adccb35afec56f8a
Vedant Kadam <vedantkd23@gmail.com>
2025-07-22 18:24:08 +0530

    wish message with event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { WishMessageComponent } from './components/wish-message/wish-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    WishMessageComponent
   ],
   imports: [
     BrowserModule,
